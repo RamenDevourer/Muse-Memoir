a87c3f8f4eeef0c3f92ffdf5a9159f1698c8b45c
RamenDevourer <priyanshuburde2004@gmail.com>
2024-05-06 14:38:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://muse-memoir.vercel.app/'
+}));
+
 
 mongoose
     .connect(mongoDBURL)
